c9bb503a3347ab8b767bf2d55d0f3f81162d694b
Dan Smith <dev@dotherightthing.co.nz>
2022-09-21 10:27:10 +1200

    Add padStart polyfill, compile into dist folder

---
+++ src/polyfills/string.polyfill.js
@@ -0,0 +1,31 @@
+/**
+ * @file Raw polyfill as the babel/core-js internal require system wasn't respected by Max
+ * @see {@link https://raw.githubusercontent.com/behnammodi/polyfill/master/string.polyfill.js}
+ */
+
+/**
+ * String.padStart()
+ * version 1.0.1
+ * Feature	        Chrome  Firefox Internet Explorer   Opera	Safari	Edge
+ * Basic support	57   	51      (No)	            44   	10      15
+ * -------------------------------------------------------------------------------
+ */
+ if (!String.prototype.padStart) {
+  Object.defineProperty(String.prototype, 'padStart', {
+    configurable: true,
+    writable: true,
+    value: function (targetLength, padString) {
+      targetLength = targetLength >> 0; //floor if number or convert non-number to 0;
+      padString = String(typeof padString !== 'undefined' ? padString : ' ');
+      if (this.length > targetLength) {
+        return String(this);
+      } else {
+        targetLength = targetLength - this.length;
+        if (targetLength > padString.length) {
+          padString += padString.repeat(targetLength / padString.length); //append to original to ensure we are longer than needed
+        }
+        return padString.slice(0, targetLength) + String(this);
+      }
+    },
+  });
+}
