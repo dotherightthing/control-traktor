f2cb9b26ca1d0368bfdd3b3e7ac1140baed8d3a7
Dan Smith <dev@dotherightthing.co.nz>
2022-09-25 21:47:30 +1300

    Simplify function style to expose functions to patcher, housekeeping, documentation

--- README.md
+++ README.md
@@ -4,15 +4,26 @@ Max4Live devices.
 
 ## renameSelectedTrack
 
-* tap a tab on Mira (iPad)
-* scroll between options on Push2
+### Description
 
-Name the track from a list of common options.
+When jamming it's easy to lose track of what each track represents. This function provides a quick way to name the track from a list of common options.
+
+### Usage
+
+1. add *M4L Helpers* to Master track
+2. map tabs object to encoder on non-Push controller
+3. select an Audio, MIDI, or Instrument track
+4. turn encoder from (2) to change the track name and see it update instantly
+5. alternatively use [Mira](https://cycling74.com/products/mira/) to view the Mira frame on an iPad, then tap a tab (option) on Mira to change the selected track name
+6. alternatively lock Push to the device on the Master track, and turn the `TRACKNAME` encoder
+
+### Roadmap
+
+* add preset or picked track colours
 
 ## resampleSelectedTrack
 
-* tap a tab on Mira (iPad)
-* map button on Push2 or other controller
+### Description
 
 When jamming with soft synths or hardware synths, MIDI notes are driving that synth. Recording into the instrument channel would therefore record the MIDI input rather than the audio output. When recorded MIDI is played back at a later date, the sound is often different to what was originally played, due to some combination of synth settings not being saved with the MIDI input (this is especially the case with hardware synths). In this instance it's better to capture the audio output rather than the MIDI input. For synths that don't have MIDI input, audio capture is the only choice.
 
@@ -39,6 +50,17 @@ Recording can now be started via a single button press on the desired clip slot.
 
 Note: This patch is functionally similar to ClyphX Pro's `INSAUDIO`/`INSMIDI`.
 
+### Usage
+
+1. add *M4L Helpers* to Master track
+2. TODO..
+* tap a tab on Mira (iPad)
+* map button on Push2 or other controller
+
+### Roadmap
+
+.
+
 ---
 
 ## Research
@@ -46,7 +68,6 @@ Note: This patch is functionally similar to ClyphX Pro's `INSAUDIO`/`INSMIDI`.
 * [Ableton Max For Live Beginner's Masterclass (Phelan Kane / CM)](https://www.youtube.com/watch?v=tkXz8sf-KwU&t=751s)
 * [Learning The Live Object Model and Creating 0dB Max (Josh Spoon / The Producer's Kitchen)](https://www.youtube.com/watch?v=agtnMQkDjUE)
 * [Traversing the Ableton Live Object Model (Jeff Merkel)](https://www.youtube.com/watch?v=qeabaagMZr8)
-* [Mira iPad app (Cycling 74)](https://cycling74.com/products/mira/)
 * [JavaScript Live API Tutorials (Adam Murray / Compusition)](http://compusition.com/writings/js-live-api)
 * [LOM - The Live Object Model (Cycling 74)](https://docs.cycling74.com/max8/vignettes/live_object_model)
 * [Max JS Require Guide (Cycling 74)](https://docs.cycling74.com/max8/vignettes/jsrequire)
@@ -190,3 +211,37 @@ function functionName(arg) {
 Links:
 
 * <https://docs.cycling74.com/max8/vignettes/jsbasic#Private_(Local)_Functions>
+
+### Mira
+
+#### Mira supports
+
+* `live.dial` (with label)
+* `live.grid`
+* `live.meter`
+* `live.number`
+* `live.slider`
+* `live.tab`
+* `live.text` (with label - button/toggle, also see prototypes)
+* `live.toggle`
+
+#### Mira does not support
+
+* `live.adsrui`
+* `live.arrows`
+* `live.comment`
+* `live.gain`
+* `live.line`
+* `live.menu`
+* `live.scope`
+* `live.step`
+
+#### Bang whem Transition from
+
+For buttons (`live.text`) MIDI mappable controls should activate on mouse down AND mouse up, to prevent 2 clicks being required to repeat the action.
+
+For buttons (`live.text`) Mira-only controls should only activate on transition from zero to one, otherwise every touch would trigger the action twice.
+
+#### Links
+
+* [Mira iPad app (Cycling 74)](https://cycling74.com/products/mira/)
--- src/m4l-helpers.js
+++ src/m4l-helpers.js
@@ -4,191 +4,51 @@
 inlets = 1;
 outlets = 1;
 
-// private functions and variables
-log.local = 1;
-getSelectedTrackObj.local = 1;
-selfOnMasterTrack.local = 1;
-
 // global functions and variables
 
 /**
- * @function log
- * @see {@link http://compusition.com/writings/js-live-selectedTrackObj-logging}
+ * @function bang
+ * @summary Runs automatically when 'live.thisdevice' left outlet is connected to 'js script-name.js' inlet
  */
-const log = function () {
-    post('------------\n');
-
-    for (let i = 0, len = arguments.length; i < len; i += 1) {
-        const message = arguments[i];
-
-        if (message && message.toString) {
-            let s = message.toString();
-
-            if (s.indexOf('[object ') >= 0) {
-                s = JSON.stringify(message);
-            }
-
-            post(s);
-        } else if (message === null) {
-            post('<null>');
-        } else {
-            post(message);
-        }
-    }
-
-    post('\n');
-};
-
-// support console.log
-const console = { log }; // eslint-disable-line no-unused-vars
-
-/**
- * @function createTrackName
- * @param {string} baseName Base name
- * @param {boolean} timeStamp Whether to output a trailing timestamp (aids debugging)
- * @returns {string} trackName
- */
-const createTrackName = function (baseName, timeStamp = true) {
-    let timeStampStr = '';
-    let trackName = '';
-
-    if (timeStamp) {
-        const date = new Date();
-        timeStampStr = `${String(date.getHours()).padStart(2, '0')}:${String(date.getMinutes()).padStart(2, '0')}`;
-    }
-
-    timeStampStr = timeStamp ? (` (${timeStampStr})`) : '';
-
-    trackName = `[${baseName}]${timeStampStr}`;
-
-    return trackName;
-};
-
-/**
- * @function getSelectedTrackObj
- * @summary Checks whether the selected track is an Audio/Midi/Instrument track and not a Return/Master track
- * @returns {object} selectedTrackObj
- */
-const getSelectedTrackObj = function () {
-    let selectedTrackObj = new LiveAPI('live_set view selected_track');
-
-    if (selectedTrackObj) {
-        const sourceTrackCanBeArmed = Boolean(Number(selectedTrackObj.get('can_be_armed')));
-
-        // Excludes return and master tracks
-        if (!sourceTrackCanBeArmed) {
-            selectedTrackObj = null;
-        }
-    }
-
-    return selectedTrackObj;
-};
-
-/**
- * @function getTrackIds
- * @returns {Array} trackIds
- */
-const getTrackIds = function () {
-    const setObj = new LiveAPI('live_set');
-
-    // setObj fails if Preview is off
-    if (!setObj) {
-        return null;
-    }
-
-    const setTracks = setObj.get('tracks');
-    const trackIds = setTracks.filter(key => key !== 'id'); // remove 'id' strings from [id,11,id,12,id,13,id,1,id,7,id,8,id,9]
-
-    return trackIds;
-};
-
-/**
- * @function getTrackInputType
- * @param {string} availableInputTypes Stringified array of objects
- * @param {symbol} sourceTrackName Name of existing track to use as the audio source
- * @returns {object} inputType
- * @see {@link https://github.com/weston-bailey/m4l-plugins/blob/067fd5b9da8350229d1539ae97a2be7f5ed6c19c/max-projects/FFX%20Freq%20Seq%20Proj/code/fsTracker.js#L116}
- */
-const getTrackInputType = function (availableInputTypes = [], sourceTrackName = '') {
-    const sourceTrackNameStr = String(sourceTrackName);
-    let trackInputType;
-    let routing = JSON.parse(availableInputTypes); // de-string
-
-    routing.available_input_routing_types.forEach((type) => {
-        if (type.display_name === sourceTrackNameStr) {
-            trackInputType = type;
-        }
-    });
-
-    return trackInputType;
-};
-
-/**
- * @function insertTrack
- * @param {string} sourceTrackId ID of existing track to insert the new track next to
- * @param {string} trackType Type of new track (audio|midi)
- * @param {string} insertPosition Position of new track relative to existing track (after|before)
- * @returns {object|null} newTrackObj
- * @todo setObj fails if Preview is off - is this expected?
- */
-const insertTrack = function (sourceTrackId, trackType = 'audio', insertPosition = 'after') {
-    const setObj = new LiveAPI('live_set');
-
-    // setObj fails if Preview is off
-    if (!setObj) {
-        return null;
-    }
-
-    const trackId = Number(sourceTrackId);
-    const trackIds = getTrackIds();
-    const trackIndex = trackIds.indexOf(trackId);
-
-    const newTrackIndex = (insertPosition === 'before') ? trackIndex : (trackIndex + 1);
-
-    setObj.call(`create_${trackType}_track`, newTrackIndex);
-
-    const newTrackObj = new LiveAPI('live_set tracks ' + newTrackIndex);
-
-    return newTrackObj;
-};
+function bang() { // eslint-disable-line no-unused-vars
+    // this_device = the Max for Live Device object that contains this JavaScript code
+    // in the max object, live.thisdevice determines when the Max Device has completely loaded
+    // and sends a bang from its left outlet when the Device is fully initialized, including the Live API).
+}
 
 /**
  * @function loadbang
  * @summary Executes when the Max patch opens
  */
-const loadbang = function () {
-    console.log('m4l-helpers initialized.');
-};
+function loadbang() {
+    console.log('m4l-helpers initialized.'); // eslint-disable-line no-console
+}
 
 /**
  * @function renameSelectedTrack
  * @summary Rename the selected track
  * @param {string} trackName Track name
  */
-const renameSelectedTrack = function (trackName) {
-    console.log('rename track to', trackName);
-
+function renameSelectedTrack(trackName) {
     const selectedTrackObj = getSelectedTrackObj();
 
-    console.log(typeof selectedTrackObj);
-
-    if (typeof selectedTrackObj !== null) { // eslint-disable-line
+    if (typeof selectedTrackObj === 'object') {
         selectedTrackObj.set('name', trackName);
     }
-};
+}
 
 /**
  * @function resampleSelectedTrack
  * @summary Resample the selected track
  * @param {string} insertPosition Insert position relative to selected track (before|after)
  */
-const resampleSelectedTrack = function (insertPosition) {
+function resampleSelectedTrack(insertPosition) {
     const onMasterTrack = selfOnMasterTrack();
 
     if (onMasterTrack) {
         const selectedTrackObj = getSelectedTrackObj();
 
-        if (typeof selectedTrackObj !== null) { // eslint-disable-line
+        if (typeof selectedTrackObj === 'object') {
             const selectedTrackHasAudioOutput = Boolean(Number(selectedTrackObj.get('has_audio_output')));
             const selectedTrackHasMidiOutput = Boolean(Number(selectedTrackObj.get('has_midi_output')));
             const selectedTrackId = selectedTrackObj.id;
@@ -202,7 +62,7 @@ const resampleSelectedTrack = function (insertPosition) {
                 newTrackType = 'midi';
             }
 
-            if (typeof newTrackType !== 'undefined') {
+            if (typeof newTrackType === 'string') {
                 const newTrackObj = insertTrack(selectedTrackId, newTrackType, insertPosition);
 
                 if (!newTrackObj || (newTrackObj === null)) {
@@ -226,14 +86,161 @@ const resampleSelectedTrack = function (insertPosition) {
             }
         }
     }
-};
+}
+
+// local functions and variables
+
+log.local = 1;
+getSelectedTrackObj.local = 1;
+selfOnMasterTrack.local = 1;
+
+// support console.log
+console = { log }; // eslint-disable-line no-unused-vars
+
+/**
+ * @function log
+ * @see {@link http://compusition.com/writings/js-live-selectedTrackObj-logging}
+ */
+function log() {
+    post('------------\n');
+
+    for (let i = 0, len = arguments.length; i < len; i += 1) {
+        const message = arguments[i];
+
+        if (message && message.toString) {
+            let s = message.toString();
+
+            if (s.indexOf('[object ') >= 0) {
+                s = JSON.stringify(message);
+            }
+
+            post(s);
+        } else if (message === null) {
+            post('<null>');
+        } else {
+            post(message);
+        }
+    }
+
+    post('\n');
+}
+
+/**
+ * @function createTrackName
+ * @param {string} baseName Base name
+ * @param {boolean} timeStamp Whether to output a trailing timestamp (aids debugging)
+ * @returns {string} trackName
+ */
+function createTrackName(baseName, timeStamp = true) {
+    let timeStampStr = '';
+    let trackName = '';
+
+    if (timeStamp) {
+        const date = new Date();
+        timeStampStr = `${String(date.getHours()).padStart(2, '0')}:${String(date.getMinutes()).padStart(2, '0')}`;
+    }
+
+    timeStampStr = timeStamp ? (` (${timeStampStr})`) : '';
+
+    trackName = `[${baseName}]${timeStampStr}`;
+
+    return trackName;
+}
+
+/**
+ * @function getSelectedTrackObj
+ * @summary Checks whether the selected track is an Audio/Midi/Instrument track and not a Return/Master track
+ * @returns {object} selectedTrackObj
+ */
+function getSelectedTrackObj() {
+    let selectedTrackObj = new LiveAPI('live_set view selected_track');
+
+    if (selectedTrackObj) {
+        const sourceTrackCanBeArmed = Boolean(Number(selectedTrackObj.get('can_be_armed')));
+
+        // Excludes return and master tracks
+        if (!sourceTrackCanBeArmed) {
+            selectedTrackObj = null;
+        }
+    }
+
+    return selectedTrackObj;
+}
+
+/**
+ * @function getTrackIds
+ * @returns {Array} trackIds
+ */
+function getTrackIds() {
+    const setObj = new LiveAPI('live_set');
+
+    // setObj fails if Preview is off
+    if (!setObj) {
+        return null;
+    }
+
+    const setTracks = setObj.get('tracks');
+    const trackIds = setTracks.filter(key => key !== 'id'); // remove 'id' strings from [id,11,id,12,id,13,id,1,id,7,id,8,id,9]
+
+    return trackIds;
+}
+
+/**
+ * @function getTrackInputType
+ * @param {string} availableInputTypes Stringified array of objects
+ * @param {symbol} sourceTrackName Name of existing track to use as the audio source
+ * @returns {object} inputType
+ * @see {@link https://github.com/weston-bailey/m4l-plugins/blob/067fd5b9da8350229d1539ae97a2be7f5ed6c19c/max-projects/FFX%20Freq%20Seq%20Proj/code/fsTracker.js#L116}
+ */
+function getTrackInputType(availableInputTypes = [], sourceTrackName = '') {
+    const sourceTrackNameStr = String(sourceTrackName);
+    let trackInputType;
+    let routing = JSON.parse(availableInputTypes); // de-string
+
+    routing.available_input_routing_types.forEach((type) => {
+        if (type.display_name === sourceTrackNameStr) {
+            trackInputType = type;
+        }
+    });
+
+    return trackInputType;
+}
+
+/**
+ * @function insertTrack
+ * @param {string} sourceTrackId ID of existing track to insert the new track next to
+ * @param {string} trackType Type of new track (audio|midi)
+ * @param {string} insertPosition Position of new track relative to existing track (after|before)
+ * @returns {object|null} newTrackObj
+ * @todo setObj fails if Preview is off - is this expected?
+ */
+function insertTrack(sourceTrackId, trackType = 'audio', insertPosition = 'after') {
+    const setObj = new LiveAPI('live_set');
+
+    // setObj fails if Preview is off
+    if (!setObj) {
+        return null;
+    }
+
+    const trackId = Number(sourceTrackId);
+    const trackIds = getTrackIds();
+    const trackIndex = trackIds.indexOf(trackId);
+
+    const newTrackIndex = (insertPosition === 'before') ? trackIndex : (trackIndex + 1);
+
+    setObj.call(`create_${trackType}_track`, newTrackIndex);
+
+    const newTrackObj = new LiveAPI('live_set tracks ' + newTrackIndex);
+
+    return newTrackObj;
+}
 
 /**
  * @function selfOnMasterTrack
  * @summary Checks whether the max device is on the Master track
  * @returns {boolean} isOnMasterTrack
  */
-const selfOnMasterTrack = function () {
+function selfOnMasterTrack() {
     let isOnMasterTrack = false;
 
     const deviceTrackObj = new LiveAPI('this_device canonical_parent');
@@ -249,14 +256,4 @@ const selfOnMasterTrack = function () {
     }
 
     return isOnMasterTrack;
-};
-
-/**
- * @function bang
- * @summary Runs automatically when 'live.thisdevice' left outlet is connected to 'js script-name.js' inlet
- */
-const bang = function () { // eslint-disable-line no-unused-vars
-    // this_device = the Max for Live Device object that contains this JavaScript code
-    // in the max object, live.thisdevice determines when the Max Device has completely loaded
-    // and sends a bang from its left outlet when the Device is fully initialized, including the Live API).
-};
+}
